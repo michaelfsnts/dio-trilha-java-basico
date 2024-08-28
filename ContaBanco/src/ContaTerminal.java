e13b3b2c6eb33ff7a690aa7883db4a41babc4f5b
michaelfsnts <michaelferreira706@gmail.com>
2024-08-28 18:03:16 -0300

    update: implantação de Exceção.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,23 +1,28 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
 
-       Scanner entrada = new Scanner(System.in);
-       System.out.print("Por favor insira o número da conta:");
-       int numero = entrada.nextInt();
-       //
-       System.out.printf("Por favor insira o número da Agência:");
-       String agencia = entrada.next(); 
-       //
-       System.out.printf("Digite o seu nome: ");
-       String nomeCliente = entrada.next();
-       //
-       System.out.print("Digite seu saldo: ");
-       double saldo = entrada.nextDouble();
+       try {
+          Scanner entrada = new Scanner(System.in);
+          System.out.print("Por favor insira o número da conta:");
+          int numero = entrada.nextInt();
+          //
+          System.out.println("Por favor insira o número da Agência:");
+          String agencia = entrada.next();
+          //
+          System.out.println("Digite o seu nome: ");
+          String nomeCliente = entrada.next();
+          //
+          System.out.println("Digite seu saldo: ");
+          double saldo = entrada.nextDouble();
 
        System.out.print("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta " + numero +" e seu saldo "+ saldo + " já está disponível para saque.");
 
        entrada.close();
+       }catch (InputMismatchException e){
+          System.out.println("Os campos não foram preenchidos corretamente.");
+       }
     }
 }
