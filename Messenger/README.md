d756d0d87a63fb1a7c226836da45b243741ccaf1
michaelfsnts <michaelferreira706@gmail.com>
2024-09-15 12:54:31 -0300

    create: doc

---
+++ Messenger/README.md
@@ -0,0 +1,10 @@
+# ESTUDOS SOBRE OS PILARES DA PROGRAMAÇÃO ORIENTADA À OBJETOS
+
+Intuito desse repositório é salvar um projeto onde sua estrutura está baseada pela formalidade imposta pela linguagem sobre os Pilares de POO (Programação Orientada à Objetos).
+
+## Conceitos utilizados
+
+    [x] ENCAPSULAMENTO;
+    [x] HERANÇA;
+    [x] ABSTRAÇÃO;
+    [x] POLIMORFISMO.
---
+++ Messenger/src/MSNMessenger.java
@@ -0,0 +1,15 @@
+public class MSNMessenger {
+ 
+    public void enviarMensagem(){
+        System.out.println("Enviando mensagem");
+    }
+    public void receberMensagem(){
+        System.out.println("Recebendo mensagem");
+    }
+    public void validarConectadoInternet(){
+        System.out.println("Validando se está conectado à internet");
+    }
+    public void salvarHistoricoMensagem(){
+        System.out.println("Salvando o histórico da mensagem");
+    }
+}
